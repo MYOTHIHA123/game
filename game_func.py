3fdaf2d385aa14d5f9b8b1b06dfcdb5f963fd809
Your Name <you@example.com>
2023-10-13 17:50:41 +0300

    rename file

---
+++ game_func.py
@@ -0,0 +1,116 @@
+import pygame as pg
+
+pg.init()
+
+screen_width = 800
+screen_height = 600
+
+display = pg.display.set_mode((screen_width,screen_height))
+
+pg.display.set_caption('Космическое вторжение')
+icon_img = pg.image.load('resources/img/ufo.png')
+pg.display.set_icon(icon_img)
+
+
+background_img = pg.image.load('resources/img/background.png')
+display.blit(background_img, (0, 0))
+
+sysfont = pg.font.SysFont('arial', 50)
+#text_img = sysfont.render(' myo thiha kyaw', True, 'red')
+#display.blit(text_img, (380, 500))
+#w = text_img.get_width()
+#h = text_img.get_height()
+'''font = pg.font.Font('resources/font/04B_19__.TTF', 48)
+game_over_img = font.render('Game Over', True, 'white')
+w = game_over_img.get_width()
+h = game_over_img.get_height()
+x = screen_width/2 - w/2
+y = screen_height/2 - h/2
+display.blit(game_over_img, (x, y))'''
+
+player_img =pg.image.load('resources/img/player.png')
+player_width = player_img.get_width()
+player_height = player_img.get_height()
+player_x = screen_width / 2 - player_width / 2
+player_y = screen_height - player_height
+player_velocity = 1
+player_dx = 0
+
+bullet_img =pg.image.load('resources/img/bullet.png')
+bullet_width = bullet_img.get_width()
+bullet_height = bullet_img.get_height()
+bullet_x = 0
+bullet_y = 0
+bullet_dy = -7
+bullet_visible = False
+
+def bullet_create():
+    global bullet_y, bullet_x, bullet_visible
+    bullet_x = player_x
+    bullet_y = player_y - bullet_height
+    bullet_visible = True
+def model_update():
+    player_model()
+    bullet_model()
+def player_model():
+    global player_x
+    player_x = player_x + player_dx
+    if player_x < 0:
+        player_x = 0
+    if player_x + player_width > screen_width:
+        player_x = screen_width - player_width
+
+def bullet_model():
+    global bullet_visible, bullet_y
+
+    if bullet_visible:
+        bullet_y = bullet_y + bullet_dy
+        if bullet_y < 0:
+            bullet_visible = False
+            print(f"{bullet_visible=}")
+
+def redraw():
+    display.blit(background_img, (0, 0))
+    display.blit(player_img, (player_x, player_y))
+    if bullet_visible:
+        display.blit(bullet_img, (bullet_x, bullet_y))
+
+    pg.display.update()
+
+def event_process():
+    global player_dx
+    running = True
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        if event.type == pg.KEYDOWN and event.key == pg.K_q:
+            running = False
+        if event.type == pg.KEYDOWN and event.key == pg.K_LEFT:
+            player_dx = - player_velocity
+            #print('Press m')
+            #display.blit(text_img, (x, y))
+        if event.type == pg.KEYDOWN and event.key == pg.K_RIGHT:
+            player_dx = player_velocity
+        if event.type == pg.KEYUP and event.key == pg.K_LEFT:
+            player_dx = 0
+        if event.type == pg.KEYUP and event.key == pg.K_RIGHT:
+            player_dx = 0
+        if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+            if not bullet_visible:
+                bullet_create()
+                print('Fire!')
+    return running
+running = True
+while running:
+    model_update()
+    redraw()
+    running = event_process()
+    #display.fill('blue',(20, 50, 100, 250))
+
+
+
+
+
+
+
+pg.quit()
